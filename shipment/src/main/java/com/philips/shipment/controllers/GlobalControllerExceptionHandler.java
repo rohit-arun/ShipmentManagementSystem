5ad7046ce56223ec6920071e06463d2ddd446625
Rohit Arun <65969016+rohit-arun@users.noreply.github.com>
2023-09-04 12:48:08 +0530

    Update GlobalControllerExceptionHandler.java

--- shipment/src/main/java/com/philips/shipment/controllers/GlobalControllerExceptionHandler.java
+++ shipment/src/main/java/com/philips/shipment/controllers/GlobalControllerExceptionHandler.java
@@ -1,16 +1,32 @@
 package com.philips.shipment.controllers;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
-@ControllerAdvice
-public class GlobalControllerExceptionHandler {
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-    @ExceptionHandler(Exception.class)
-    public ModelAndView handleException(Exception ex) {
-        ModelAndView modelAndView = new ModelAndView("errorPage");
-        modelAndView.addObject("errorMessage", "An error occurred: " + ex.getMessage());
-        return modelAndView;
+@Controller
+public class GlobalControllerExceptionHandler implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error500";
+            } else if (statusCode == HttpStatus.BAD_REQUEST.value())
+                return "error400";
+
+        }
+        return "errorPage";
     }
 }
